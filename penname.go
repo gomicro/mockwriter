1ded8a282ac7b761a9acdc77605625a2a7ec30d7
Daniel Hess <dan9186@gmail.com>
2016-01-28 23:15:53 -0800

    getting rid of write files and merging with main

--- penname.go
+++ penname.go
@@ -14,3 +14,12 @@ func New() *PenName {
 func (p *PenName) ReturnError(err error) {
 	p.returnError = err
 }
+
+func (p *PenName) Write(b []byte) (n int, err error) {
+	if p.returnError != nil {
+		return 0, p.returnError
+	}
+
+	p.Written = b
+	return len(p.Written), nil
+}
--- penname_test.go
+++ penname_test.go
@@ -8,7 +8,7 @@ import (
 	. "github.com/onsi/gomega"
 )
 
-func TestPenName(t *testing.T) {
+func TestPenname(t *testing.T) {
 	g := Goblin(t)
 	RegisterFailHandler(func(m string, _ ...int) { g.Fail(m) })
 
@@ -21,6 +21,26 @@ func TestPenName(t *testing.T) {
 			_, err := p.Write([]byte("I'm trying to do something"))
 			Expect(err).To(HaveOccurred())
 			Expect(err.Error()).To(Equal(errText))
+		})
+	})
+
+	g.Describe("Writing", func() {
+		g.It("should record what is written to it", func() {
+			t := "Nothing to see here move along"
+			p := &PenName{}
+
+			n, err := p.Write([]byte(t))
+			Expect(err).NotTo(HaveOccurred())
+			Expect(n).To(Equal(len(t)))
+			Expect(p.Written).To(Equal([]byte(t)))
+		})
+
+		g.It("should function as a writer", func() {
+			t := "Nothing to see here move along"
+			p := &PenName{}
+
+			fmt.Fprint(p, t)
+			Expect(p.Written).To(Equal([]byte(t)))
 		})
 	})
 }
